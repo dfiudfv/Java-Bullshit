038f8f7ca0d5521b9ba40c755b37ed01df005944
_isolate <ivankrotov910@gmail.com>
2025-10-20 15:46:25 +0300

    Initial commit 1

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("\n");
+        System.out.println("Hello world");
+        System.out.println("i want some sports\n");
+
+        final double maxSpeed = 120;
+        System.out.println("max speed: " + maxSpeed);
+        int num = 10;
+        num = 12;
+        System.out.println("num = " + num);
+        boolean isRain = false;
+        System.out.println("isRain = " + isRain);
+        char
+
+    }
+}
